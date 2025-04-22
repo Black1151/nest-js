17d794cdf2284882af3846afa1ca8659f0482b39
Black1151 <danblack9988@gmail.com>
2025-04-22 13:57:09 +0100

    implement permission group dnd

--- insight-fe/src/app/(main)/(protected)/administration/permission-group-manager/_components/PermissionGroupManagerPageClient.tsx
+++ insight-fe/src/app/(main)/(protected)/administration/permission-group-manager/_components/PermissionGroupManagerPageClient.tsx
@@ -3,18 +3,22 @@
 import { ContentGrid } from "@/components/ContentGrid";
 import { useState } from "react";
 import PermissionGroupListTable from "./sections/permission-group-list-table/PermissionGroupListTable";
+import { PermissionGroupPermissionDnd } from "./sections/permission-group-permission-dnd/PermissionGroupPermissionDnd";
 
 export const PermissionGroupManagerPageClient = () => {
   const [selectedPermissionGroupId, setSelectedPermissionGroupId] = useState<
-    string | null
+    number | null
   >(null);
 
   return (
     <>
       <ContentGrid gridTemplateColumns="1fr 1fr">
         <PermissionGroupListTable
-          setSelectedPermissionGroupId={setSelectedPermissionGroupId}
+          setSelectedPermissionGroupId={(id) =>
+            setSelectedPermissionGroupId(Number(id))
+          }
         />
+        <PermissionGroupPermissionDnd groupId={selectedPermissionGroupId} />
       </ContentGrid>
     </>
   );
