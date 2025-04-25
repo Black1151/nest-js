95d08f8e15d9e283d2b757b920029eb359b89cf5
Black1151 <danblack9988@gmail.com>
2025-04-25 18:22:16 +0100

    update perm conditioanl rendering invocations to array

--- insight-fe/src/app/(main)/(protected)/administration/user-manager/UserManagerPageClient.tsx
+++ insight-fe/src/app/(main)/(protected)/administration/user-manager/UserManagerPageClient.tsx
@@ -7,25 +7,38 @@ import { UserDetailSection } from "./_components/sections/user/user-details-sect
 import { UserRolesSection } from "./_components/sections/user/user-role-section/UserRolesSection";
 import { useAuth } from "@/context/AuthContext";
 import { RequirePermission } from "@/rbac/RequirePermission";
+import { redirect } from "next/navigation";
 
 export function UserManagerPageClient() {
   const [selectedUserPublicId, setSelectedUserPublicId] = useState<
     string | null
   >(null);
 
+  const { userPermissions } = useAuth();
+
+  if (!userPermissions.includes("user.getAllUsers")) {
+    redirect("/unauthorised");
+  }
+
   return (
     <>
       <ContentGrid gridTemplateColumns="1fr 1fr">
         <UserListTable setSelectedUserPublicId={setSelectedUserPublicId} />
 
-        <RequirePermission permission="user.get">
+        <RequirePermission permissions={["user.getUserByPublicId"]}>
           <UserDetailSection
             publicId={selectedUserPublicId}
             setSelectedUserPublicId={setSelectedUserPublicId}
           />
         </RequirePermission>
 
-        <RequirePermission permission="user.getRolesForUser">
+        <RequirePermission
+          permissions={[
+            "user.getRolesForUser",
+            "user.updateUserRolesFromArray",
+            "role.findAllRole",
+          ]}
+        >
           <UserRolesSection publicId={selectedUserPublicId} />
         </RequirePermission>
       </ContentGrid>
