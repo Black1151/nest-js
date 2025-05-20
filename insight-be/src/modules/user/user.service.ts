2cd9f36ad45c1340bd6a2621e733d59a264fa6fe
Black1151 <73132619+Black1151@users.noreply.github.com>
2025-05-20 18:21:43 +0100

    Load user profiles when listing users

--- insight-be/src/modules/user/user.service.ts
+++ insight-be/src/modules/user/user.service.ts
@@ -34,6 +34,10 @@ export class UsersService {
     return this.userRepository.find({
       skip: offset,
       take: limit,
+      relations: {
+        studentProfile: true,
+        educatorProfile: true,
+      },
     });
   }
 
