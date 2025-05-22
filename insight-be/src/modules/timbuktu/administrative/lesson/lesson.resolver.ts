788e175478ea3a15b80c503e3e30b1988456b3c7
Black1151 <danblack9988@gmail.com>
2025-05-22 13:27:58 +0100

    add permission key decorator

--- insight-be/src/modules/timbuktu/administrative/lesson/lesson.resolver.ts
+++ insight-be/src/modules/timbuktu/administrative/lesson/lesson.resolver.ts
@@ -3,6 +3,7 @@ import { createBaseResolver } from 'src/common/base.resolver';
 import { CreateLessonInput, UpdateLessonInput } from './lesson.inputs';
 import { LessonEntity } from './lesson.entity';
 import { LessonService } from './lesson.service';
+import { RbacPermissionKey } from 'src/modules/rbac/decorators/resolver-permission-key.decorator';
 
 const BaseLessonResolver = createBaseResolver<
   LessonEntity,
@@ -28,6 +29,7 @@ export class LessonResolver extends BaseLessonResolver {
     super(lessonService);
   }
 
+  @RbacPermissionKey('lesson.lessonsByTopic')
   @Query(() => [LessonEntity])
   async lessonsByTopic(
     @Args('topicId', { type: () => ID }) topicId: string,
