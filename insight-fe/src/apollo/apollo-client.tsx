e2f853ec229e3fe9eb4703112772d81e8c1cb5f4
Black1151 <73132619+Black1151@users.noreply.github.com>
2025-06-06 08:30:54 +0100

    feat(fe): add token refresh link to Apollo client

--- insight-fe/src/apollo/apollo-client.tsx
+++ insight-fe/src/apollo/apollo-client.tsx
@@ -1,9 +1,30 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  from,
+  onError,
+} from "@apollo/client";
+import { ensureRefresh } from "../refreshClient";
+
+const httpLink = new HttpLink({
+  uri: "/api/graphql",
+  credentials: "include",
+});
+
+const refreshLink = onError(({ graphQLErrors, networkError, forward, operation }) => {
+  const isAuthError =
+    (networkError as any)?.statusCode === 401 ||
+    graphQLErrors?.some(({ extensions }) => extensions?.code === "UNAUTHENTICATED");
+
+  if (isAuthError) {
+    return ensureRefresh().then(() => forward(operation));
+  }
+});
 
 const client = new ApolloClient({
-  uri: "/api/graphql",
+  link: from([refreshLink, httpLink]),
   cache: new InMemoryCache(),
-  credentials: "include",
 });
 
 export default client;
