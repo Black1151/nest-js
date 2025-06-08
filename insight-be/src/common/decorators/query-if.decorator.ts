27031df7870a427e2f2c56acd8b698dec748441c
Black1151 <73132619+Black1151@users.noreply.github.com>
2025-06-08 19:27:01 +0100

    Skip RBAC checks for disabled resolver methods

--- insight-be/src/common/decorators/query-if.decorator.ts
+++ insight-be/src/common/decorators/query-if.decorator.ts
@@ -1,5 +1,7 @@
 // src/common/decorators/query-if.decorator.ts
 import { Query } from '@nestjs/graphql';
+import { SetMetadata } from '@nestjs/common';
+import { IS_DISABLED_OPERATION_KEY } from './disabled-operation.decorator';
 
 /**
  * Usage:
@@ -21,5 +23,11 @@ export function QueryIf(
     target: any,
     propertyKey: string | symbol,
     descriptor: PropertyDescriptor,
-  ) => {};
+  ) => {
+    SetMetadata(IS_DISABLED_OPERATION_KEY, true)(
+      target,
+      propertyKey,
+      descriptor,
+    );
+  };
 }
