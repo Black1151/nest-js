fc2fa311beeaea48b694dc29431267b2d22d248a
Black1151 <73132619+Black1151@users.noreply.github.com>
2025-06-11 14:10:40 +0100

    fix: include ThemeEntity repository in LessonModule

--- insight-be/src/modules/timbuktu/administrative/lesson/lesson.module.ts
+++ insight-be/src/modules/timbuktu/administrative/lesson/lesson.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LessonService } from './lesson.service';
 import { LessonResolver } from './lesson.resolver';
 import { LessonEntity } from './lesson.entity';
+import { ThemeEntity } from '../theme/theme.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([LessonEntity])],
+  imports: [TypeOrmModule.forFeature([LessonEntity, ThemeEntity])],
   providers: [LessonService, LessonResolver],
   exports: [LessonService],
 })
