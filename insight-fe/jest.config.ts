20fad515d588bcc9b79b3abf34cad47423008a65
Black1151 <73132619+Black1151@users.noreply.github.com>
2025-06-13 07:24:56 +0100

    test: add theme builder component tests

---
+++ insight-fe/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@generated/(.*)$': '<rootDir>/src/generated/$1',
+  },
+};
+
+export default config;
