ba8bd7ac16c26d645c68c85122048a99f5f6d5a7
Black1151 <danblack9988@gmail.com>
2025-06-16 05:47:07 +0100

    create lesson viewer page

---
+++ insight-fe/src/app/(main)/(protected)/students/lesson-viewer/LessonViewerPageClient.tsx
@@ -0,0 +1,13 @@
+import { VStack } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+
+export const LessonViewerPageClient = () => {
+  return (
+    <VStack>
+      <HStack>
+        <Text>Lesson Viewer</Text>
+      </HStack>
+    </VStack>
+  );
+};
---
+++ insight-fe/src/app/(main)/(protected)/students/lesson-viewer/page.tsx
@@ -0,0 +1,5 @@
+import { LessonViewerPageClient } from "./LessonViewerPageClient";
+
+export default function LessonViewerPage() {
+  return <LessonViewerPageClient />;
+}
