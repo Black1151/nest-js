36996d3f27c157aa86a5acc8116bad6e406a2bb1
Black1151 <73132619+Black1151@users.noreply.github.com>
2025-06-26 08:58:12 +0100

    feat: add color tokens to style collection input

--- insight-be/src/modules/timbuktu/administrative/style-collection/style-collection.inputs.ts
+++ insight-be/src/modules/timbuktu/administrative/style-collection/style-collection.inputs.ts
@@ -4,10 +4,15 @@ import { Field, ID, InputType, PartialType } from '@nestjs/graphql';
 export class CreateStyleCollectionInput {
   @Field()
   name: string;
+
+  @Field(() => [String], { nullable: true })
+  colorTokens?: string[];
 }
 
 @InputType()
-export class UpdateStyleCollectionInput extends PartialType(CreateStyleCollectionInput) {
+export class UpdateStyleCollectionInput extends PartialType(
+  CreateStyleCollectionInput,
+) {
   @Field(() => ID)
   id: number;
 }
